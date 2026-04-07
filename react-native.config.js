45a9b6a5d5e430297473e9cfc294186074c65f60
humuraelvin <humura53@gmail.com>
2026-04-07 14:49:58 +0200

    fixed ffmpeg ios issue

--- react-native.config.js
+++ react-native.config.js
@@ -4,16 +4,5 @@ module.exports = {
     android: {
       packageName: 'com.ihirwe.talynksocial'
     }
-  },
-  dependencies: {
-    'ffmpeg-kit-react-native': {
-      platforms: {
-        // Android is handled by Expo config plugin + patch-package.
-        android: null,
-        // iOS ffmpeg-kit upstream binaries currently return 404 during pod install.
-        // Disable iOS autolinking to prevent pod install failure in EAS iOS builds.
-        ios: null
-      }
-    }
   }
 };
